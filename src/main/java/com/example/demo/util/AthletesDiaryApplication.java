1d4ad92b67b36f123fe48970e958b5bc67625750
Vít Kubánek <vit10101050@gmail.com>
2024-12-10 21:27:10 +0100

    .

---
+++ src/main/java/com/example/demo/util/AthletesDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthletesDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthletesDiaryApplication.class, args);
+    }
+
+}
